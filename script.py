9a82c78180cf7b1b8e4b38209347b46a90b5a64e
chupino <mauricioescalante903@gmail.com>
2024-09-15 18:03:29 -0500

    a

--- script.py
+++ script.py
@@ -1,7 +1,12 @@
 from dask.distributed import Client
+import dask.dataframe as dd
 
 # Reemplaza <IP_DEL_SCHEDULER> con la IP pública del nodo del scheduler
 client = Client('tcp://0.tcp.ngrok.io:18682')
 
 # Verifica la conexión
 print(client)
+
+df = dd.read_csv('./u.data' , delim_whitespace=True)
+
+print(df.head())
