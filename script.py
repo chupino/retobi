634eb6f5d4c0743d903fb89302a91411e2f4b952
chupino <mauricioescalante903@gmail.com>
2024-09-15 18:43:32 -0500

    a

--- script.py
+++ script.py
@@ -1,5 +1,8 @@
 from dask.distributed import Client
 import dask.dataframe as dd
+import urllib.request
+import os
+
 
 # Reemplaza <IP_DEL_SCHEDULER> con la IP pública del nodo del scheduler
 client = Client('tcp://8.tcp.ngrok.io:19538')
@@ -7,6 +10,15 @@ client = Client('tcp://8.tcp.ngrok.io:19538')
 # Verifica la conexión
 print(client)
 
-df = dd.read_csv('./u.data' , delim_whitespace=True)
+# Define la URL del archivo y la ruta local para guardarlo
+url = 'https://raw.githubusercontent.com/chupino/retobi/main/u.data'
+local_filename = 'u.data'
 
+# Descargar el archivo
+urllib.request.urlretrieve(url, local_filename)
+
+# Leer el archivo en un DataFrame de Dask
+df = dd.read_csv(local_filename, delim_whitespace=True)
+
+# Mostrar las primeras filas del DataFrame
 print(df.head())
